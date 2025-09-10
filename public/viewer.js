b0046bc76a5cd70c2fe9fda99bb6a09d1533d226
Bisma Abdurachman Tauhid <Everidea.tech@gmail.com>
2025-09-10 10:34:29 +0700

    Readjust watermark position

--- public/viewer.js
+++ public/viewer.js
@@ -134,14 +134,12 @@
       watermarkMesh.visible = true;
 
       const positionX = 0;
-      const positionY = 0;
+      const positionY = -0.5;
       const positionZ = -1;
       watermarkMesh.position.set(positionX, positionY, positionZ);
       
       // Menggunakan renderOrder yang tinggi untuk memastikan selalu di atas
       watermarkMesh.renderOrder = 999;
-      
-      //camera.add(watermarkMesh);
 
       scene.add(watermarkMesh);
       
