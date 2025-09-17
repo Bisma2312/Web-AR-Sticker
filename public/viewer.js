ce1d908636ad761df3558835cbfd8831c0b388cf
Bisma Abdurachman Tauhid <Everidea.tech@gmail.com>
2025-09-17 15:32:39 +0700

    Reposition Watermark

--- public/viewer.js
+++ public/viewer.js
@@ -145,7 +145,7 @@
       watermarkMesh.visible = true;
 
       const positionX = 0;
-      const positionY = -0.35;
+      const positionY = -0.5;
       const positionZ = -1;
       watermarkMesh.position.set(positionX, positionY, positionZ);
       
